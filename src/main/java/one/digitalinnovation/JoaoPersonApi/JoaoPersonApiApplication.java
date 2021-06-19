0c216790a03da8d707e92d7058422c95a32db61d
João Marcos Soares <jomamts@gmail.com>
2021-06-19 09:34:12 -0300

    feat: project setup from spring initializr

---
+++ src/main/java/one/digitalinnovation/JoaoPersonApi/JoaoPersonApiApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.JoaoPersonApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoaoPersonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoaoPersonApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/digitalinnovation/JoaoPersonApi/JoaoPersonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.JoaoPersonApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoaoPersonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
