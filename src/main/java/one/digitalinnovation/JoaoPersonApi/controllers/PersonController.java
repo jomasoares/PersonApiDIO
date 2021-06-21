e295bc6dc1cac6bb43818977dc93eacef57d0d14
João Marcos Soares <jomamts@gmail.com>
2021-06-21 08:17:07 -0300

    feat: add POST route to PersonController

--- src/main/java/one/digitalinnovation/JoaoPersonApi/controllers/PersonController.java
+++ src/main/java/one/digitalinnovation/JoaoPersonApi/controllers/PersonController.java
@@ -1,15 +1,37 @@
 package one.digitalinnovation.JoaoPersonApi.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import one.digitalinnovation.JoaoPersonApi.dto.response.MessageResponseDTO;
+import one.digitalinnovation.JoaoPersonApi.entities.Person;
+import one.digitalinnovation.JoaoPersonApi.services.PersonService;
 
 @RestController
 @RequestMapping("api/v1/person")
 public class PersonController {
-    
+
+    private PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService){
+        this.personService = personService;
+    }
+
     @GetMapping
     public String getPerson() {
         return "API test!";
     } 
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO createPerson(@RequestBody Person person){
+        return personService.create(person);
+    }
 }
