e11511b95f1888e357a08153b95f7d9d30c054a6
João Marcos Soares <jomamts@gmail.com>
2021-06-21 16:47:08 -0300

    feat: add list all and findById functionalities

--- src/main/java/one/digitalinnovation/JoaoPersonApi/controllers/PersonController.java
+++ src/main/java/one/digitalinnovation/JoaoPersonApi/controllers/PersonController.java
@@ -1,10 +1,13 @@
 package one.digitalinnovation.JoaoPersonApi.controllers;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import one.digitalinnovation.JoaoPersonApi.dto.request.PersonDTO;
 import one.digitalinnovation.JoaoPersonApi.dto.response.MessageResponseDTO;
+import one.digitalinnovation.JoaoPersonApi.exceptions.NotFoundException;
 import one.digitalinnovation.JoaoPersonApi.services.PersonService;
 
 @RestController
@@ -27,8 +31,8 @@ public class PersonController {
     }
 
     @GetMapping
-    public String getPerson() {
-        return "API test!";
+    public List<PersonDTO> listAll() {
+        return personService.listAll();
     } 
 
     @PostMapping
@@ -36,5 +40,10 @@ public class PersonController {
     public MessageResponseDTO createPerson(@RequestBody  @Valid PersonDTO personDTO){
         // System.out.println("olaaaaaaaaaaa MUNDOOOOOOO!");
         return personService.create(personDTO);
+    }
+
+    @GetMapping("/{id}")
+    public PersonDTO findById(@PathVariable Long id) throws NotFoundException {
+        return personService.findById(id);
     }
 }
--- src/main/java/one/digitalinnovation/JoaoPersonApi/services/PersonService.java
+++ src/main/java/one/digitalinnovation/JoaoPersonApi/services/PersonService.java
@@ -1,5 +1,8 @@
 package one.digitalinnovation.JoaoPersonApi.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,11 +10,14 @@ import one.digitalinnovation.JoaoPersonApi.dto.mappers.PersonMapper;
 import one.digitalinnovation.JoaoPersonApi.dto.request.PersonDTO;
 import one.digitalinnovation.JoaoPersonApi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.JoaoPersonApi.entities.Person;
+import one.digitalinnovation.JoaoPersonApi.exceptions.NotFoundException;
 import one.digitalinnovation.JoaoPersonApi.repositories.PersonRepository;
 
 @Service
 public class PersonService {
     private PersonRepository personRepository;
+
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
 
     @Autowired
     public PersonService(PersonRepository personRepository){
@@ -19,11 +25,25 @@ public class PersonService {
     }
 
     public MessageResponseDTO create(PersonDTO personDTO) {
-        Person person = PersonMapper.INSTANCE.toModel(personDTO);
+        Person person = personMapper.toModel(personDTO);
         Person savedPerson = personRepository.save(person);
         return MessageResponseDTO
         .builder()
         .message("created person with ID " + savedPerson.getId())
         .build();
+    }
+
+    public List<PersonDTO> listAll() {
+        List<Person> people = personRepository.findAll();
+        return people.stream()
+            .map(personMapper::toDTO)
+            .collect(Collectors.toList());
+    }
+
+    public PersonDTO findById(Long id) throws NotFoundException {
+        Person person = personRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Person", id));
+
+        return personMapper.toDTO(person);
     }
 }
