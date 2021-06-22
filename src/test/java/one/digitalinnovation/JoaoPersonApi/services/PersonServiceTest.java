26b6756edc7c77d66e1d9eacef87581f9d1a245e
João Marcos Soares <jomamts@gmail.com>
2021-06-22 14:16:50 -0300

    feat: add unit tests

---
+++ src/test/java/one/digitalinnovation/JoaoPersonApi/services/PersonServiceTest.java
@@ -0,0 +1,51 @@
+package one.digitalinnovation.JoaoPersonApi.services;
+
+import one.digitalinnovation.JoaoPersonApi.dto.mappers.PersonMapper;
+import one.digitalinnovation.JoaoPersonApi.dto.request.PersonDTO;
+import one.digitalinnovation.JoaoPersonApi.dto.response.MessageResponseDTO;
+import one.digitalinnovation.JoaoPersonApi.entities.Person;
+import one.digitalinnovation.JoaoPersonApi.repositories.PersonRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static one.digitalinnovation.JoaoPersonApi.utils.PersonUtils.createFakeDTO;
+import static one.digitalinnovation.JoaoPersonApi.utils.PersonUtils.createFakeEntity;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class PersonServiceTest {
+
+    @Mock
+    private PersonRepository personRepository;
+
+    @Mock
+    private PersonMapper personMapper;
+
+    @InjectMocks
+    private PersonService personService;
+
+    @Test
+    void testGivenPersonDTOThenReturnSuccessSavedMessage() {
+        PersonDTO personDTO = createFakeDTO();
+        Person expectedSavedPerson = createFakeEntity();
+
+        when(personRepository.save(any(Person.class))).thenReturn(expectedSavedPerson);
+
+        MessageResponseDTO expectedSuccessMessage = createExpectedSuccessMessage(expectedSavedPerson.getId());
+        MessageResponseDTO successMessage = personService.create(personDTO);
+
+        assertEquals(expectedSuccessMessage, successMessage);
+    }
+
+    private MessageResponseDTO createExpectedSuccessMessage(Long savedPersonId) {
+        return MessageResponseDTO.builder()
+                .message("Person successfully created with ID " + savedPersonId)
+                .build();
+    }
+
+}
